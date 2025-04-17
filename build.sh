274e9191fda55ffa717190708cd918b8b8130f13
Peter Schuelke <pschuelke@phase2technology.com>
2025-04-16 17:02:08 -0700

    Update github-pages build.

--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+# Clean up any previous build
+rm -rf dist
+rm -rf web/dist
+
 # Build the Redwood app
 yarn rw build
 
 # Create the dist directory if it doesn't exist
-mkdir -p web/dist
+mkdir -p dist
 
 # Create a .nojekyll file to tell GitHub Pages not to use Jekyll
 touch web/dist/.nojekyll
 
-# Copy the built files to the root directory
-cp -r web/dist/* .
+# Copy the built files to the dist directory
+cp -r web/dist/* dist/
 
 # Clean up
 rm -rf web/dist
