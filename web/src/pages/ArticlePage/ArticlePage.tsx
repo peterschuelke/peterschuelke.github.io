28aa6207d286e2632c162fba3547611143dbd90a
Peter Schuelke <peter.c.schuelke@gmail.com>
2025-04-21 11:50:12 -0700

    Update lint fixes.

--- web/src/pages/ArticlePage/ArticlePage.tsx
+++ web/src/pages/ArticlePage/ArticlePage.tsx
@@ -1,16 +1,35 @@
 import { Metadata } from '@redwoodjs/web'
-import ArticleCell from 'src/components/ArticleCell'
+import { useQuery } from '@redwoodjs/web'
+import { gql } from '@apollo/client'
+
+import { Success as ArticleSuccess } from 'src/components/ArticleCell/ArticleCell'
 
 interface Props {
   id: number
 }
 
 const ArticlePage = ({ id }: Props) => {
+  const { data, loading, error } = useQuery(gql`
+    query FindArticleQuery($id: Int!) {
+      article: post(id: $id) {
+        id
+        title
+        body
+        createdAt
+      }
+    }
+  `, {
+    variables: { id }
+  })
+
+  if (loading) return <div>Loading...</div>
+  if (error) return <div>Error: {error.message}</div>
+
   return (
-    <>
+    <div className="article-page">
       <Metadata title="Article" description="Article page" />
-      <ArticleCell id={id} />
-    </>
+      <ArticleSuccess article={data?.article} />
+    </div>
   )
 }
 
