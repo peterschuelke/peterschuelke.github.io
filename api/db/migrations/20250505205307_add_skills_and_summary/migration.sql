a519645c0f2f30215c0cc5deb7eee75ba3a0d725
Peter Schuelke <peter.c.schuelke@gmail.com>
2025-05-05 14:55:46 -0700

    Update project schema.

---
+++ api/db/migrations/20250505205307_add_skills_and_summary/migration.sql
@@ -0,0 +1,22 @@
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN "summary" TEXT;
+
+-- CreateTable
+CREATE TABLE "Skill" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_ProjectToSkill" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+    CONSTRAINT "_ProjectToSkill_A_fkey" FOREIGN KEY ("A") REFERENCES "Project" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_ProjectToSkill_B_fkey" FOREIGN KEY ("B") REFERENCES "Skill" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ProjectToSkill_AB_unique" ON "_ProjectToSkill"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ProjectToSkill_B_index" ON "_ProjectToSkill"("B");
--- api/src/graphql/projects.sdl.ts
+++ api/src/graphql/projects.sdl.ts
@@ -1,38 +1,66 @@
 export const schema = gql`
+  type Skill {
+    id: Int!
+    title: String!
+    image: String
+    projects: [Project]!
+  }
+
   type Project {
     id: Int!
     title: String!
     description: String!
+    summary: String
     image: String!
-    link: String
+    link: String!
     role: String!
+    skills: [Skill!]
     createdAt: DateTime!
   }
 
   type Query {
     projects: [Project!]! @requireAuth
     project(id: Int!): Project @requireAuth
+    skills: [Skill!]! @requireAuth
+    skill(id: Int!): Skill @requireAuth
   }
 
   input CreateProjectInput {
     title: String!
     description: String!
+    summary: String
     image: String!
-    link: String
+    link: String!
     role: String!
+    skillIds: [Int!]
   }
 
   input UpdateProjectInput {
     title: String
     description: String
+    summary: String
     image: String
     link: String
     role: String
+    skillIds: [Int!]
+  }
+
+  input CreateSkillInput {
+    title: String!
+    image: String
+  }
+
+  input UpdateSkillInput {
+    title: String
+    image: String
   }
 
   type Mutation {
     createProject(input: CreateProjectInput!): Project! @requireAuth
     updateProject(id: Int!, input: UpdateProjectInput!): Project! @requireAuth
     deleteProject(id: Int!): Project! @requireAuth
+    createSkill(input: CreateSkillInput!): Skill! @requireAuth
+    updateSkill(id: Int!, input: UpdateSkillInput!): Skill! @requireAuth
+    deleteSkill(id: Int!): Skill! @requireAuth
   }
 `
