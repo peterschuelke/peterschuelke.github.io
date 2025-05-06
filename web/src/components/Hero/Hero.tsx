36f2063d8ebfc5de7d2bc77d037f176582248c22
Peter Schuelke <peter.c.schuelke@gmail.com>
2025-05-05 21:27:23 -0700

    Add Hero Component.

---
+++ web/src/components/Hero/Hero.tsx
@@ -0,0 +1,12 @@
+
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1 className="hero__title">PETER SCHUELKE</h1>
+      <p className="hero__subtitle">SOFTWARE ENGINEER</p>
+    </div>
+  )
+}
+
+export default Hero
--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,16 @@
 // import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 import ProjectsList from 'src/components/Project/ProjectsList'
+import Hero from 'src/components/Hero/Hero'
+
 import './HomePage.pcss'
 
 const HomePage = () => {
   return (
     <div className="home-page">
       <Metadata title="Home" description="Home page" />
-      <h1>Peter Schuelke's Portfolio</h1>
-      <h2>Projects</h2>
+      <Hero/>
+      <h2>My Work</h2>
       <ProjectsList />
     </div>
   )
